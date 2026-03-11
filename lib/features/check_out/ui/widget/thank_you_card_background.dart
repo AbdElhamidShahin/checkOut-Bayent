7e5b21e86786cc27aafc753fa574f19ecdff6b7b
Abdelhamid shahin <abdelhamid.shahin0@gmail.com>
2026-03-11 14:37:57 +0200

    Added ThankYou Screen Added thank you content added 2 image Update app router Update payment details

---
+++ lib/features/check_out/ui/widget/thank_you_card_background.dart
@@ -0,0 +1,151 @@
+import 'package:checkout_payment_ui/features/check_out/ui/widget/thank_you_content.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+
+class ThankYouCardBackground extends StatelessWidget {
+  const ThankYouCardBackground({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.arrow_back,
+                  size: 50.sp,
+                )),
+            SizedBox(
+              height: 70.h,
+            ),
+          ],
+        ),
+        Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Container(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height * .80,
+              decoration: BoxDecoration(
+                color: const Color(0xFFEDEDED),
+                borderRadius: BorderRadius.circular(20.r),
+              ),
+            ),
+            Positioned(
+              top: 60.h,
+              left: 0,
+              right: 0,
+              bottom: 150.h + 40.r,
+              child: ThankYouContentBody(),
+            ),
+            Positioned(
+              bottom: 40.h,
+              left: 20.w,
+              right: 20.w,
+              child: BarcodeAndPaidStatus(),
+            ),
+            Positioned(
+              bottom: 150.h,
+              left: -20.w,
+              child: CircleAvatar(
+                radius: 20.r,
+                backgroundColor: Colors.white,
+              ),
+            ),
+            Positioned(
+              bottom: 150.h,
+              right: -20.w,
+              child: CircleAvatar(
+                radius: 20.r,
+                backgroundColor: Colors.white,
+              ),
+            ),
+            Positioned(
+              bottom: 170.h,
+              left: 25.w,
+              right: 25.w,
+              child: CustomPaint(
+                painter: DashedLinePainter(),
+              ),
+            ),
+            Positioned(
+              top: -60.h,
+              left: 0,
+              right: 0,
+              child: CircleAvatar(
+                radius: 60.r,
+                backgroundColor: const Color(0xFFEDEDED),
+                child: CircleAvatar(
+                  radius: 50.r,
+                  backgroundColor: const Color(0xFF34A853),
+                  child: Icon(Icons.check, color: Colors.white, size: 75.sp),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class DashedLinePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint()
+      ..color = const Color(0xFFB8B8B8)
+      ..strokeWidth = 2;
+
+    double dashWidth = 5, dashSpace = 5, startX = 0;
+    while (startX < size.width) {
+      canvas.drawLine(Offset(startX, 0), Offset(startX + dashWidth, 0), paint);
+      startX += dashWidth + dashSpace;
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
+}
+
+class BarcodeAndPaidStatus extends StatelessWidget {
+  const BarcodeAndPaidStatus({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Image.asset(
+            width: 140.w,
+            height: 70.h,
+            "assets/images/SVGRepo_iconCarrier.png"),
+        Container(
+          width: 115.w,
+          height: 60.h,
+          decoration: ShapeDecoration(
+            shape: RoundedRectangleBorder(
+              side: const BorderSide(width: 1.50, color: Color(0xFF34A853)),
+              borderRadius: BorderRadius.circular(15.r),
+            ),
+          ),
+          child: Center(
+            child: Text(
+              'PAID',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: const Color(0xFF34A853),
+                fontSize: 24.sp,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
---
+++ lib/features/check_out/ui/widget/thank_you_content.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+
+class ThankYouContentBody extends StatelessWidget {
+  const ThankYouContentBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(top: 36.h, left: 20.w, right: 20.w),
+      child: Column(
+        children: [
+          Text("Thank you!",
+              style: TextStyle(fontSize: 25.sp, fontWeight: FontWeight.w500)),
+          Text("Your transaction was successful",
+              style: TextStyle(fontSize: 18.sp, color: Colors.black54)),
+          SizedBox(height: 32.h),
+          const PaymentItemInfo(title: "Date", value: "01/24/2023"),
+          const PaymentItemInfo(title: "Time", value: "10:15 AM"),
+          const PaymentItemInfo(title: "To", value: "Sam Louis"),
+          Divider(height: 32.h, thickness: 1.5),
+          const PaymentItemInfo(
+              title: "Total", value: r"$50.97", isTotal: true),
+          SizedBox(height: 20.h),
+          // Payment Card Info Container
+          Container(
+            padding: EdgeInsets.all(16.w),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(15.r),
+            ),
+            child: Row(
+              children: [
+              SvgPicture.asset("assets/images/logo.svg"),
+                SizedBox(width: 15.w),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Credit Card", style: TextStyle(fontSize: 16.sp)),
+                    Text("Mastercard **78",
+                        style: TextStyle(color: Colors.grey, fontSize: 14.sp)),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class PaymentItemInfo extends StatelessWidget {
+  final String title, value;
+  final bool isTotal;
+  const PaymentItemInfo(
+      {super.key,
+      required this.title,
+      required this.value,
+      this.isTotal = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 4.h),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(title,
+              style: TextStyle(
+                  fontSize: isTotal ? 24.sp : 18.sp,
+                  fontWeight: isTotal ? FontWeight.bold : FontWeight.w400)),
+          Text(value,
+              style: TextStyle(
+                  fontSize: isTotal ? 24.sp : 18.sp,
+                  fontWeight: isTotal ? FontWeight.bold : FontWeight.w600)),
+        ],
+      ),
+    );
+  }
+}
