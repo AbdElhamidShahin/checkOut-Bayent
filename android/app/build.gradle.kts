3781ec845013ef4e3fd750e7aa7ab8e42a8c0dc7
Abdelhamid shahin <abdelhamid.shahin0@gmail.com>
2026-03-16 16:13:31 +0200

    Added Stripe Package

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "com.example.checkout_payment_ui"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 21
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -34,6 +34,7 @@ android {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
+            isMinifyEnabled = true
             signingConfig = signingConfigs.getByName("debug")
         }
     }
